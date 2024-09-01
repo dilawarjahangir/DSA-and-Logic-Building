2662af73d3b0032f72b7d749e7595a047a9c1026
Muhammad Dilawar Jahangir <dilawarjahangir2@gmail.com>
2024-09-01 18:15:00 +0500

    patterns Completed

--- 01-Patterns/JavaScript/02-patterns.js
+++ 01-Patterns/JavaScript/02-patterns.js
@@ -110,7 +110,159 @@ const ZeroOneTriangle = () => {
   }
 };
 
-ZeroOneTriangle();
+// ZeroOneTriangle();
+
+// col   01234567
+//row 0 *      *
+//row 1 **    **
+//row 2 ***  ***
+//row 3 ********
+//row 4 ********
+//row 5 ***  ***
+//row 6 **    **
+//row 7 *      *
+const butterFly = () => {
+  let row = 8;
+  let col = 8;
+  let half = row / 2;
+  let right = 1;
+  let left = col - 2;
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (j < right || j > left) {
+        process.stdout.write("*");
+      } else {
+        process.stdout.write(" ");
+      }
+    }
+    console.log("");
+
+    if (i < half - 1) {
+      right++;
+      left--;
+    } else if (i >= half) {
+      right--;
+      left++;
+    }
+  }
+};
+
+//   butterFly();
+
+
+
+
+
+    // *****
+  //  ***** 
+  // *****  
+//  *****   
+// *****    
+
+const rombus = () => {
+    size=5
+    print=5
+  for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= (size*2)-1; j++) {
+        if(j>=print && j<print+5 ){
+            process.stdout.write("*")
+        }else{
+            process.stdout.write(" ")
+        }
+      }
+      print--
+    console.log()
+  }
+};
+
+// rombus()
+
+
+
+
+
+//     *****
+//    *   * 
+//   *   *  
+//  *   *   
+// *****    
+const hollowRombus = () => {
+  size=5
+  print=5
+for (let i = 1; i <= size; i++) {
+  for (let j = 1; j <= (size*2)-1; j++) {
+      if(j>=print && j<print+5 ){
+        if(i===1 ||  i==5 ||  j==print || j===print+4 ){
+          process.stdout.write("*")
+
+        }
+        else{
+
+          process.stdout.write(" ")
+        }
+      }else{
+          process.stdout.write(" ")
+      }
+    }
+    print--
+  console.log()
+}
+};
+
+// hollowRombus()
+
+
+
+
+
+
+//    *   
+//   ***  
+//  ***** 
+// *******
+//  ***** 
+//   ***  
+//    *   
+const diamond=()=>{
+ size=8
+  left=8/2
+  right=8/2
+  for(let i=1;i<size; i++){
+    for(let j=1;j<size; j++){
+      if(j>=left && j<=right ){
+
+        process.stdout.write("*")      
+      }else{
+        
+        process.stdout.write(" ")      
+      }
+
+    } 
+    
+
+    if(i>=4){
+      left++
+      right--
+    }
+    else{
+      left--
+    right++ 
+    }
+
+    console.log()
+  }
+}
+
+diamond()
+
+
+
+
+
+
+
+
 
 const show = () => {
   for (let i = 0; i < 5; i++) {
